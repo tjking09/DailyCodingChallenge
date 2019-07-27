822de48f9101ef638fc2a1f30db3ced3ceef03ae
Tyler <tjhockey09@gmail.com>
2019-07-27 14:00:30 -0400

    Single Number Daily Challenge

---
+++ SingleNumber/SingleNumber.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Given a non-empty array of integers, every element appears twice except for one. 
+ * Find that single one.
+ * 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SingleNumber
+{
+    class SingleNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 2, 1 };
+            var unique = new List<int>();
+
+            foreach (int i in nums)
+            {
+                if (unique.Contains(i))
+                {
+                    unique.Remove(i);
+                }
+                else
+                {
+                    unique.Add(i);
+                }
+            }
+
+            int answer = unique.First();
+            Console.WriteLine($"The unique number is {answer}");
+            Console.ReadLine();
+        }
+    }
+}
