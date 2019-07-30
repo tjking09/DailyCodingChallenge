61fbedc8245c4e023e5eaa6853492efc2f1c9eab
Tyler <tjhockey09@gmail.com>
2019-07-29 21:29:56 -0400

    Code Challenge to move zeros in an array of n length, to the end of the array while keeping the order of the rest of the array.

---
+++ MoveZeros/MoveZeros.cs
@@ -0,0 +1,48 @@
+﻿// Given an array nums, 
+// write a function to move all 0's to the end of it 
+// while maintaining the relative order of the non-zero elements.
+
+using System;
+using System.Collections.Generic;
+
+namespace MoveZeros
+{
+    class MoveZeros
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 3, 12 };
+            List<int> ans = new List<int>();
+
+            int numZeros = 0;
+            int j = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    numZeros++;
+                }
+                else
+                {
+                    ans.Add(nums[i]);
+                    j++;
+                }
+            }
+
+            for (int k = 0; k < nums.Length; k++)
+            {
+                if (k < ans.Count)
+                {
+                    nums[k] = ans[k];
+                }
+                else
+                {
+                    nums[k] = 0;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
